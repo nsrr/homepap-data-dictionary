ab9f22638f1458fcea93478e329643135022fc52
Remo Mueller <remosm@gmail.com>
2017-03-27 16:00:20 -0400

    Checking variable units now allows comparison against `nil` units

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -9,16 +9,21 @@ class DictionaryTest < Minitest::Test
   # iterators that can be used to write custom tests
   include Spout::Helpers::Iterators
 
-  VALID_UNITS = ['beats per minute', 'percent', 'events per hour', 'kilograms per meters squared', 'kilograms', 'days since enrollment', 'centimeters', 'millimeters of mercury', 'times','  ','movements','bpm','cmH2O','nights','% of days','days','liters/minute','',
-    'minutes','cm','events per hour','%','years','hours','events','seconds','mmHG','kg','attempts','items'] #Example ['mmHG','bpm','readings','minutes','%','hours','MET']
+  VALID_UNITS = [
+    ' ', '', 'beats per minute', 'percent', 'events per hour',
+    'kilograms per meters squared', 'kilograms', 'days since enrollment',
+    'centimeters', 'millimeters of mercury', 'times', 'movements', 'bpm',
+    'cmH2O', 'nights', '% of days', 'days', 'liters/minute', 'minutes', 'cm',
+    'events per hour', '%', 'years', 'hours', 'events', 'seconds', 'mmHG', 'kg',
+    'attempts', 'items'
+  ] # Example ['mmHG', 'bpm', 'readings', 'minutes', '%', 'hours', 'MET']
 
-  @variables.select{|v| ['numeric','integer'].include?(v.type)}.each do |variable|
-    define_method("test_units: "+variable.path) do
-      message = "\"#{variable.units}\"".colorize( :red ) + " invalid units.\n" +
+  @variables.select { |v| %w(numeric integer).include?(v.type) }.each do |variable|
+    define_method("test_units: #{variable.path}") do
+      message = "\"#{variable.units}\"".colorize(:red) + " invalid units.\n" +
                 "             Valid types: " +
-                VALID_UNITS.sort.collect{|u| u.inspect.colorize( :white )}.join(', ')
+                VALID_UNITS.sort_by(&:to_s).collect { |u| u.inspect.colorize(:white) }.join(', ')
       assert VALID_UNITS.include?(variable.units), message
     end
   end
-
 end
